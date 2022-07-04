6f1369a38a76a240004df66dd5317db4b9d449bd
Rabah <rmerzoug471@gmail.com>
2022-07-04 18:13:20 -0500

    passed the test

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+}
+ 
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+
+function  destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function  destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Broom) {
+    const newcats = [...cats, 'Broom'];
+    return newcats;
+}
+
+function prependCat(Arnold) {
+    const newcats = ['Arnold', ...cats];
+    return newcats;
+}
+
+function  removeLastCat() {
+    const newcats = cats.slice(0,2);
+    return newcats;
+}
+
+function removeFirstCat() {
+    const newcats = cats.slice(1);
+    return newcats;
+}
